3f179b7eea17f25420793ef6e96eb5b9d2e83e23
Quentin Warnant <lequentin@gmail.com>
2021-11-19 23:07:36 +0000

    Adding Clamp function & PI value  to AstroTools::Maths

--- AstroDX12/Src/Maths/MathUtils.h
+++ AstroDX12/Src/Maths/MathUtils.h
@@ -8,6 +8,8 @@ namespace AstroTools
 {
 	namespace Maths
 	{
+		constexpr float Pi = XM_PI;
+
 		static XMFLOAT4X4 Identity4x4()
 		{
 			constexpr XMFLOAT4X4 Identity = XMFLOAT4X4(
@@ -16,6 +18,11 @@ namespace AstroTools
 				0.0f, 0.0f, 1.0f, 0.0f,
 				0.0f, 0.0f, 0.0f, 1.0f);
 			return Identity;
+		}
+
+		static float Clamp(float x, float min, float max)
+		{
+			return x < min ? min : (x > max ? max : x);
 		}
 	};
 }
